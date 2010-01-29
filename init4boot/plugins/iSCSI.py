12ed1e7ebcec17f04830f120631768a08142fc8f
flonatel <flonatel@df17dcda-ed48-0410-9878-ce9a3bf78c96>
2010-01-29 16:19:24 +0000

    Added support for iSCSI chap; fixed a bug in iSCSI handling

--- init4boot/plugins/iSCSI.py
+++ init4boot/plugins/iSCSI.py
@@ -1,7 +1,7 @@
 #
 # init4boot iSCSI plugin
 #
-# (c) 2008-2009 by flonatel
+# (c) 2008,2010 by flonatel
 #
 # For licencing details see COPYING
 #
@@ -45,6 +45,37 @@ if [ "${boot_type}" = "iscsi" ]; then
   log "Starting iscsid"
   mkdir -p /etc/iscsi
   echo "InitiatorName=$localiqn" >/etc/iscsi/initiatorname.iscsi
+
+  cat >/etc/iscsi/iscsid.conf <<EOF
+node.startup = manual
+EOF
+
+  clp_usechap=`istrue ${usechap}`
+  if [ ${clp_usechap} -eq "1" ]; then
+    cat >>/etc/iscsi/iscsid.conf <<EOF
+node.session.auth.authmethod = CHAP
+EOF
+    if [ -n ${un_out} ]; then
+      cat >>/etc/iscsi/iscsid.conf <<EOF
+node.session.auth.username = ${un_out}
+node.session.auth.password = ${pw_out}
+EOF
+    fi
+    if [ -n ${un_in} ]; then
+      cat >>/etc/iscsi/iscsid.conf <<EOF
+node.session.auth.username_in = ${un_in}
+node.session.auth.password_in = ${pw_in}
+EOF
+    fi
+    if [ -n ${disc_un_out} ]; then
+      cat >>/etc/iscsi/iscsid.conf <<EOF
+discovery.sendtargets.auth.authmethod = CHAP
+discovery.sendtargets.auth.username = ${disc_un_out}
+discovery.sendtargets.auth.password = ${disc_pw_out}
+EOF
+    fi
+  fi
+
   # Start iscsid with -f which logs to stdout / stderr instead of
   # syslog (which is not available at the time of boot).
   /bin/iscsid -d 0 -f >/tmp/iscsid-stdout.log 2>/tmp/iscsid-stderr.log &
@@ -84,10 +115,13 @@ fi
         class PrepareRootDir:
             def output(self, ofile):
                 ofile.write("""
-logp "Postmount iSCSI setup"
-mkdir -p ${rootmnt}/etc/iscsi
-cp -a /etc/iscsi ${rootmnt}/etc
-logpe
+if [ "${boot_type}" = "iscsi" ]; then
+    logp "Postmount iSCSI setup"
+    mkdir -p ${rootmnt}/etc/iscsi
+    cp -a /etc/iscsi ${rootmnt}/etc
+    log "Finished iSCSI setup"
+    logpe
+fi
 """)
         return PrepareRootDir()
 
