ca8dabd117b3c8f90fb1e2b4fe296c11557c5b98
Andreas Florath <andreas@florath.net>
2015-10-04 13:37:15 +0200

    Added fix that Debian 'now' uses hiracical tree for libs

--- init4boot/lib/ldd.py
+++ init4boot/lib/ldd.py
@@ -11,6 +11,7 @@
 # the machine.)
 #
 # (c) 2008 by flonatel (sf@flonatel.org)
+# (c) 2015 by Andreas Florath (andreas@florath.net)
 #
 # For licensing details see COPYING
 #
@@ -19,52 +20,66 @@ import re
 import sys
 import popen2
 import os
+import fnmatch
+import logging
 
-#
-# Returns a full list of all shared libraries that are needed for the
-# prog to run.
-# The prog itself must be the full path of the program.
-#
+logger = logging.getLogger(__name__)
+
 def ldd_single(prog):
-    rlist = []
+    """Returns a full list of all shared libraries that are needed for the
+    prog to run.
+    The prog itself must be the full path of the program."""
+
+    rset = set()
     needre = re.compile("  NEEDED\s*(\S*).*")
     cout, cin = popen2.popen2("objdump -p %s" % prog)
     for l in cout:
         m = needre.match(l)
         if m:
-            rlist.append(m.group(1))
-    return rlist
+            rset.add(m.group(1))
+    return rset
+
+def find_lib(lib, libdirs):
+    """Find a lib in the dirtrees specified by libdirs."""
+    result = set()
+    for libdir in libdirs:
+        for root, dirs, files in os.walk(libdir):
+            for name in files:
+                if lib == name:
+                    result.add(os.path.join(root, name))
+    return result
 
 def ldd(prog, libdirs = ["/usr/lib", "/lib"]):
+    """Finds all the libraries the given program depends on.
+    
+    The implementation is not that straight forward, because 
+    libraries can depend on other libraries"""
+
+    # This is the set that is returned
     fileset = set()
-    libstodo = set()
+    # The set of libraries to check
+    # (this can be extended when libraries depend on other libraries)
+    libstodo = ldd_single(prog)
+    # These are already handled
     libsdone = set()
 
-    # Look for the program
-    liblist = ldd_single(prog)
-    libstodo.update(liblist)
-
     while len(libstodo) > 0:
-        e = libstodo.pop()
-        if e in libsdone:
+        lib = libstodo.pop()
+        if lib in libsdone:
             continue
         # Check where to find the lib.
-        found_lib = False
-        for lib in libdirs:
-            fplib = os.path.join(lib, e)
-            if os.path.exists(fplib):
-                fileset.add(fplib)
-                libsdone.add(e)
-                liblist = ldd_single(fplib)
-                libstodo.update(liblist)
-                found_lib = True
-
-        if found_lib == False:
-            print "Library '%s' not found" % e
+        libpaths = find_lib(lib, libdirs)
+        if libpaths == None:
+            print("Library '%s' for program '%s' not found" % (e, prog))
             sys.exit(1)
+            
+        fileset.update(libpaths)
+        libsdone.add(lib)
+
+        for nlib in libpaths:
+            libstodo.update(ldd_single(nlib))
 
     return fileset
-                
 
 #
 # Small test: call 
@@ -73,6 +88,6 @@ def ldd(prog, libdirs = ["/usr/lib", "/lib"]):
 #  python ldd.py /bin/ls
 #
 if __name__=="__main__":
-    print ldd_single(sys.argv[1])
-
-    print ldd(sys.argv[1])
+    logger.debug("Hello")
+    print(ldd_single(sys.argv[1]))
+    print(ldd(sys.argv[1]))
